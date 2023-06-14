9636c0bf0c5de4e5598b63b64e3becc411f876d6
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-06-14 13:39:20 +0100

    resumelatest

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -43,7 +43,7 @@ const Intro = () => {
           <div>
             <a
               className="i-btn"
-              href="https://drive.google.com/file/d/1bHtLy5auaZw6stpTWKxF95BrXmLHXigR/view?usp=sharing"
+              href="https://drive.google.com/file/d/10f4eA3wBriXc8nTb8JgpAhXaEeuLJgiL/view?usp=sharing"
              
               target="_blank"
             >
